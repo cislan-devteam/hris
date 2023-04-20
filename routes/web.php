787175f258178ccd295fe632d806655a2835b972
Seraval <102270689+Seraval@users.noreply.github.com>
2023-04-20 18:18:53 +0800

    template added.-w/ deans

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuperAdmin\SuperAdmin;
+use App\Http\Controllers\HR_Admin\HR_Admin;
+use App\Http\Controllers\IT_Admin\IT_Admin;
+use App\Http\Controllers\Director\Director;
+use App\Http\Controllers\Client\Client;
+use App\Http\Controllers\Employee\Employee;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +23,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::group(['middleware' => 'role:Super Admin', 'prefix' => 'superAdmin', 'as' => 'superAdmin.'], function() {
+        Route::resource('dashboard', SuperAdmin::class);
+    });
+
+    Route::group(['middleware' => 'role:HR Admin', 'prefix' => 'hrAdmin', 'as' => 'hrAdmin.'], function() {
+        Route::resource('dashboard', HR_Admin::class);
+    });
+
+    Route::group(['middleware' => 'role:IT Admin', 'prefix' => 'itAdmin', 'as' => 'itAdmin.'], function() {
+        Route::resource('dashboard', IT_Admin::class);
+    });
+
+    Route::group(['middleware' => 'role:Director', 'prefix' => 'director', 'as' => 'director.'], function() {
+        Route::resource('dashboard', Director::class);
+    });
+
+    Route::group(['middleware' => 'role:Client', 'prefix' => 'client', 'as' => 'client.'], function() {
+        Route::resource('dashboard', Client::class);
+    });
+
+    Route::group(['middleware' => 'role:Employee', 'prefix' => 'employee', 'as' => 'employee.'], function() {
+        Route::resource('dashboard', Employee::class);
+    });
 });
+
+
