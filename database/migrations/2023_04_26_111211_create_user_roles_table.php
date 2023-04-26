8edc2000a417b70e347579454b8db93296e67f4c
Dean Voltaire Tumangan <dv.tumangan@gmail.com>
2023-04-26 19:34:22 +0800

    Changes in adding User-Role

--- database/migrations/2023_04_26_111211_create_user_roles_table.php
+++ database/migrations/2023_04_26_111211_create_user_roles_table.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+*
+* Author: Dean Voltaire Tumangan
+* Email: dv.tumangan@gmail.com
+*
+*/
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
