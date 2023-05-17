82db0ba657baeee5a38a62ce0c8e565751be8734
SophiaAguilar15 <sophia.aguilar@devteam.com>
2023-05-17 08:42:14 +0000

    added new command

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -8,6 +8,7 @@ sed -i "s|APP_URL=http://localhost|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
 sed -i "s|APP_URL=https://|APP_URL=https://80-|g" .env
 sed -i "s|DB_HOST=127.0.0.1|DB_HOST=mysql|g" .env
 sed -i "s|REDIS_HOST=127.0.0.1|REDIS_HOST=redis|g" .env
+./vendor/bin/sail artisan config:clear
 echo 'SEEDING DATABASE'
 ./vendor/bin/sail artisan key:generate
 # manually run this command -> ./vendor/bin/sail artisan migrate:fresh --seed
