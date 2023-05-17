575085d27321d2f0a5939a5e99695dddb416210c
SophiaAguilar15 <sophia.aguilar@devteam.com>
2023-05-17 07:40:51 +0000

    Fixed app url

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -4,10 +4,10 @@ cp .env.example .env
 echo 'BUILDING CONTAINERS USING LARAVEL SAIL'
 ./vendor/bin/sail build —no-cache
 ./vendor/bin/sail up -d
-sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
-sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
+sed -i "s|APP_URL=http://localhost|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
+sed -i "s|APP_URL=https://|APP_URL=https://80-|g" .env
 sed -i "s|DB_HOST=127.0.0.1|DB_HOST=mysql|g" .env
 sed -i "s|REDIS_HOST=127.0.0.1|REDIS_HOST=redis|g" .env
 echo 'SEEDING DATABASE'
 ./vendor/bin/sail artisan key:generate
-./vendor/bin/sail artisan migrate:fresh --seed
+# manually run this command -> ./vendor/bin/sail artisan migrate:fresh --seed
