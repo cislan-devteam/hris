19c0d50e85d5908f355ddb42c19350f96761acb8
James <jamesdareladvincula@gmail.com>
2023-06-01 14:58:26 +0800

    Add sample user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
+        
+        $this->call(UsersTableSeeder::class);
         $this->call(RoleSeeder::class);
     }
 }
