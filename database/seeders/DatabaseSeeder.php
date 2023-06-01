b9983015c9b55364e9f2696a8df0e274ffb7463f
James <jamesdareladvincula@gmail.com>
2023-06-01 17:34:08 +0800

    Sample User and Employee Information

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         
-        $this->call(UsersTableSeeder::class);
         $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(Employee_Information_Seeder::class);
     }
 }
